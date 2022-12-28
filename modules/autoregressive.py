f126a11f6b971e355c02848dbf980b493530aac4
Siddharth Mishra-Sharma <smsharma@mit.edu>
2022-12-28 01:11:22 -0500

    Rewrite MAF bijector

--- modules/autoregressive.py
+++ modules/autoregressive.py
@@ -6,7 +6,7 @@ from flax.linen.module import compact
 import flax.linen as nn
 from flax.linen.dtypes import promote_dtype
 import distrax
-from typing import Any, List
+from typing import Any, List, Tuple
 import dataclasses
 
 Array = Any
@@ -56,14 +56,13 @@ class MaskedDense(nn.Dense):
 
 
 class MADE(nn.Module):
-    bijector_fn: Any
     n_params: Any = 2
     n_context: Any = 0
     hidden_dims: List[int] = dataclasses.field(default_factory=lambda: [32, 32])
     activation: str = "tanh"
 
     @compact
-    def __call__(self, y: Array, context=None) -> tfb.Bijector:
+    def __call__(self, y: Array, context=None):
 
         assert self.n_params == y.shape[-1]
 
@@ -91,5 +90,46 @@ class MADE(nn.Module):
 
         # Only take the values corresponding to the parameters of interest for scale and shift
         params = params[..., self.n_context :, :]
+        return params
 
-        return self.bijector_fn(params)
+
+class MAF(distrax.Bijector):
+    def __init__(self, bijector_fn, unroll_loop=False):
+
+        super().__init__(event_ndims_in=1)
+
+        self.autoregressive_fn = bijector_fn
+        self.unroll_loop = unroll_loop
+
+    def forward_and_log_det(self, x):
+
+        event_ndims = int(np.prod(x.shape[1:]))
+
+        if self.unroll_loop:
+            y = jnp.zeros_like(x)
+            log_det = None
+
+            for _ in range(event_ndims):
+                params = self.autoregressive_fn(y)
+                shift, log_scale = params[..., 0], params[..., 1]
+                y, log_det = distrax.ScalarAffine(shift=shift, log_scale=log_scale).forward_and_log_det(x)
+
+        else:
+
+            def update_fn(i, y_and_log_det):
+                y, log_det = y_and_log_det[0], y_and_log_det[1]
+                params = self.autoregressive_fn(y)
+                shift, log_scale = params[..., 0], params[..., 1]
+                y, log_det = distrax.ScalarAffine(shift=shift, log_scale=log_scale).forward_and_log_det(x)
+                return y, log_det
+
+            y, log_det = jax.lax.fori_loop(0, event_ndims, update_fn, (jnp.zeros_like(x), jnp.zeros_like(x)))
+
+        return y, log_det.sum(-1)
+
+    def inverse_and_log_det(self, y):
+        params = self.autoregressive_fn(y)
+        shift, log_scale = params[..., 0], params[..., 1]
+        x, log_det = distrax.ScalarAffine(shift=shift, log_scale=log_scale).inverse_and_log_det(y)
+
+        return x, log_det.sum(-1)
--- modules/utils.py
+++
@@ -1,7 +0,0 @@
-from tensorflow_probability.substrates import jax as tfp
-
-tfb = tfp.bijectors
-
-
-def make_shift_and_scale(params):
-    return tfb.Shift(params[..., 0])(tfb.Scale(log_scale=params[..., 1]))
